57ad9f4d71a587c7985b55e204943cea10e39756
Gopinath Suresh <gopinathsuresh@gopinaths-air.attlocal.net>
2020-05-12 23:26:18 -0500

    Added Header.js and ProjectItem.js

--- ppmtool-react-client/src/component/Dashboard.js
+++ ppmtool-react-client/src/component/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import ProjectItem from "../Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to Dashboard first react component</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
       </div>
     );
   }
