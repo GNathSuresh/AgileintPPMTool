7064d9fd2ef7fc872a95ef750c0001a730131ee1
Gopinath Suresh <gopinathsuresh@gopinaths-air.attlocal.net>
2020-05-16 07:02:29 -0500

    React Function Component, Add Project Button

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,14 +4,19 @@ import "./App.css";
 import Dashboard from "./component/Dashboard";
 import Header from "./Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashBoard" component={Dashboard}></Route>
+          <Route exact path="/addProject" component={AddProject}></Route>
+        </div>
+      </Router>
     );
   }
 }
